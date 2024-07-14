a426a952af3a69e6fa573501e724b340c068bc71
abhinavbalsora <130883778+abhinavbalsora@users.noreply.github.com>
2024-07-14 14:22:51 +0530

    Task 1
    Create an interactive navigation menu that changes color or style when scrolled or when hovering over a menu item. The navigation menu should have a fixed position and be visible on all pages. Use HTML to structure the menu, CSS to style it, and JavaScript to add interactivity, such as changing the background color or font color of the menu when it is scrolled or when a menu item is hovered over.

---
+++ PRODIGY_WD_01/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navigation = document.getElementById('navigation');
+  
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 50) {
+        navigation.style.background = '#96a88c'; // Change background color when scrolled
+      } else {
+        navigation.style.background = 'transparent'; // Reset background color
+      }
+    });
+  });
+  
+ 
